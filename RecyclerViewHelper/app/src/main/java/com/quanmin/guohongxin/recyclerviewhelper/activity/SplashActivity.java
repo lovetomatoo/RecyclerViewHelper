205880e5ad2b162b932ead3a9958a789c5898710
lovetomatoo <736685433@qq.com>
2017-04-17 16:06:47 +0800

    splash change
    splash change

--- RecyclerViewHelper/app/src/main/java/com/quanmin/guohongxin/recyclerviewhelper/activity/SplashActivity.java
+++ RecyclerViewHelper/app/src/main/java/com/quanmin/guohongxin/recyclerviewhelper/activity/SplashActivity.java
@@ -1,10 +1,15 @@
 package com.quanmin.guohongxin.recyclerviewhelper.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.quanmin.guohongxin.recyclerviewhelper.R;
 
@@ -21,6 +26,18 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -28,6 +45,5 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 1000);
-
     }
 }
